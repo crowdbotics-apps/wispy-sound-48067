a0193f6b0ef555d8c6ea4c5599852d3ae304b049
Crowdbotics <team@crowdbotics.com>
2024-05-22 21:10:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-sound-48067",
+  url: "https://wispy-sound-48067.botics.co",
+  api: "https://wispy-sound-48067.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
